858d843fe522260c31c0bc5dabb5619d49b60c60
Vinícius <viniscrv@gmail.com>
2022-10-31 17:39:11 -0300

    criado estagios de jogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import Game from './components/Game/Game'
+import GameOver from './components/GameOver/GameOver'
 import StartScreen from './components/StartScreen/StartScreen'
+import { wordsList } from './data/words'
+
+
+const stages = [
+  {id: 1, name: 'start'},
+  {id: 2, name: 'game'},
+  {id: 3, name: 'end'},
+]
 
 function App() {
 
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+
   return (
     <div className="App">
-      <StartScreen/>
+      {gameStage === 'start' && <StartScreen/>}
+      {gameStage === 'game' && <Game/>}
+      {gameStage === 'end' && <GameOver/>}
     </div>
   )
 }
