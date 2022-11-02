64d37943bd2f3daa0f8d85af00ad9fdd06c66b97
Vinícius <viniscrv@gmail.com>
2022-11-02 13:48:45 -0300

    checado letras certas e erradas

--- src/App.jsx
+++ src/App.jsx
@@ -53,8 +53,21 @@ function App() {
     setGameStage(stages[1].name);
   };
 
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
+  const verifyLetter = (letter) => {
+    const normalizedLetter = letter.toLowerCase();
+
+    if(guessedLetters.includes(normalizedLetter) || wrongLetters.includes(normalizedLetter)) {
+      return;
+    }
+    if(letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters, normalizedLetter
+      ])
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters, normalizedLetter
+      ])
+    }
   };
 
   const retry = () => {
--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { Container } from "./style";
 
-const Game = ({ verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score }) => {
+const Game = ({
+  verifyLetter,
+  pickedWord,
+  pickedCategory,
+  letters,
+  guessedLetters,
+  wrongLetters,
+  guesses,
+  score,
+}) => {
+  const [letter, setLetter] = useState("");
+
+  const letterInputRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    verifyLetter(letter);
+    setLetter("");
+    letterInputRef.current.focus();
+  }
+
   return (
     <Container>
       <p>
@@ -13,18 +33,28 @@ const Game = ({ verifyLetter, pickedWord, pickedCategory, letters, guessedLetter
       </h3>
       <p>Você ainda tem {guesses} tentativa(s).</p>
       <div className="wordContainer">
-        {letters.map((letter, i)=> (
+        {letters.map((letter, i) =>
           guessedLetters.includes(letter) ? (
-            <span key={i} className="letter">{letter}</span>
+            <span key={i} className="letter">
+              {letter}
+            </span>
           ) : (
             <span key={i} className="blank"></span>
           )
-        ))}
+        )}
       </div>
       <div className="letterContainer">
         <p>Tente adivinhar uma letra da palavra</p>
-        <form>
-          <input type="text" name="letter" maxLength="1" required />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="letter"
+            maxLength="1"
+            required
+            onChange={(e) => setLetter(e.target.value)}
+            value={letter}
+            ref={letterInputRef}
+          />
           <button>Jogar</button>
         </form>
       </div>
